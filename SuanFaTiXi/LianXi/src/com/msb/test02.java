f4bb26d4eb329ab936297ab745a181f70cc0e6b9
liukaiwen1234 <3177453878@qq.com>
2022-11-28 20:01:06 +0800

    test

---
+++ SuanFaTiXi/LianXi/src/com/msb/test02.java
@@ -0,0 +1,14 @@
+package com.msb;
+
+/**
+ * @Auther: Liu kw
+ * @Date: 2022/11/28 - 11 - 28 - 19:59
+ * @Description: com.msb
+ * @version: 1.0
+ */
+public class test02 {
+
+    public static void main(String[] args) {
+        System.out.println("1");
+    }
+}
